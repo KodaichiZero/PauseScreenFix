d72c1d4e33bef5fcdec791b313732fc803d4b112
Captain McScroggo <shutupyoufool@gmail.com>
2021-06-14 20:08:25 -0400

    Add project files.

---
+++ Pause Screen Deselect Remover/FPSInputModulePatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PSF {
+	[HarmonyPatch(typeof(FPSInputModule), "ProcessMousePress")]
+	class FPSInputModulePatch {
+		static bool Prefix(ref PointerInputModule.MouseButtonEventData data, ref FPSInputModule __instance) {
+			PointerEventData buttonData = data.buttonData;
+			GameObject gameObject = buttonData.pointerCurrentRaycast.gameObject;
+			if(gameObject == null && __instance.lastGroup != null && __instance.lastGroup is IngameMenu) {
+				return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ Pause Screen Deselect Remover/PSFMod.cs
@@ -0,0 +1,17 @@
+﻿// Include this namespace to simplify the code below
+using HarmonyLib;
+using QModManager.API.ModLoading;
+
+namespace PSF {
+    // Your main patching class must have the QModCore attribute (and must be public)
+    [QModCore]
+    public static class PSFMod {
+        // Your patching method must have the QModPatch attribute (and must be public)
+        [QModPatch]
+        public static void PSFInit() {
+            // Add your patching code here
+            Harmony harmony = new Harmony("PauseScreenFix");
+            harmony.PatchAll();
+        }
+    }
+}
